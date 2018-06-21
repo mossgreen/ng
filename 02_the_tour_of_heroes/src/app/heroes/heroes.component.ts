9d3886d4be8ad9dfc009913a6e019ecd329b1fb3
Moss GU <gufeifei@outlook.com>
2018-06-21 19:59:16 +1200

    split up cmps

--- 02_the_tour_of_heroes/src/app/heroes/heroes.component.ts
+++ 02_the_tour_of_heroes/src/app/heroes/heroes.component.ts
@@ -11,7 +11,7 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  selectedHero: Hero;
+  hero: Hero;
 
   constructor() { }
 
@@ -19,7 +19,7 @@ export class HeroesComponent implements OnInit {
   }
 
   onSelect(hero: Hero) {
-    this.selectedHero = hero;
+    this.hero = hero;
   }
 
 }
