b7b2ba0ac44edc5f1e0d0d8bd38649b9c082b77e
Moss GU <gufeifei@outlook.com>
2018-07-15 20:42:56 +1200

    clean code

--- 02_the_tour_of_heroes/src/app/app-routing.module.ts
+++ 02_the_tour_of_heroes/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroesComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- 02_the_tour_of_heroes/src/app/heroes/heroes.component.ts
+++ 02_the_tour_of_heroes/src/app/heroes/heroes.component.ts
@@ -9,11 +9,9 @@ import {HeroService} from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) {
-
   }
 
   ngOnInit() {
@@ -24,9 +22,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
 }
