23d5589c896694e7439cbc2f994f61801d7acba8
Moss GU <gufeifei@outlook.com>
2018-07-17 18:27:26 +1200

    install in memory api

--- 02_the_tour_of_heroes/src/app/app.module.ts
+++ 02_the_tour_of_heroes/src/app/app.module.ts
@@ -8,19 +8,21 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    MessagesComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
