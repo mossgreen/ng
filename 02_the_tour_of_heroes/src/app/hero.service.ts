524111fd75e0959904720d261dbca69743693255
Moss GU <gufeifei@outlook.com>
2018-07-17 19:34:32 +1200

    delete

--- 02_the_tour_of_heroes/src/app/hero.service.ts
+++ 02_the_tour_of_heroes/src/app/hero.service.ts
@@ -71,4 +71,25 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`added hero id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+
+  }
+
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url,hero, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
