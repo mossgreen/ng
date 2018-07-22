7649497e8313344a5446540edb82fd191dff9f0e
Moss GU <gufeifei@outlook.com>
2018-07-23 09:59:18 +1200

    ng

--- 02_the_tour_of_heroes/src/app/hero.service.ts
+++ 02_the_tour_of_heroes/src/app/hero.service.ts
@@ -92,4 +92,16 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+        tap(_ => this.log(`found heroes matching "${term}`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+  }
 }
